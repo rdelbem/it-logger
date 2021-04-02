0c8b3455d5488e3cb8e3d4995b9387062c002a64
Rodrigo Del Bem <rodrigodelbem@gmail.com>
2021-04-01 22:12:21 -0300

    modals and alert

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import M from "materialize-css/dist/js/materialize.min.js";
 
 import SearchBar from "./components/layout/SearchBar";
 import Logs from "./components/logs/Logs";
+import AddBtn from "./components/layout/AddBtn";
+import AddLogModal from "./components/logs/AddLogModal";
+
 import "./App.css";
 
 function App() {
@@ -17,6 +20,8 @@ function App() {
     <>
       <SearchBar />
       <div className="container">
+        <AddBtn />
+        <AddLogModal />
         <Logs />
       </div>
     </>
